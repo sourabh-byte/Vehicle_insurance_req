ed4085603313b9ac7f92e4e85cc66b1f3744a808
sourabh-byte <sourabhshashidhar@gmail.com>
2025-07-04 19:11:59 +0530

    venv created and packages installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sourabh Turamari",
+    author_email="sourabhshashidhar@gmail.com",
+    packages=find_packages()
+)
